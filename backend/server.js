86f8d935144abd308192cfca90f73f47e448aeed
m736 <megaladevagi@gmail.com>
2023-07-14 18:40:56 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
 const corsOptions = {
-  origin: "https://creo-8w4j.onrender.com/", // frontend URI (ReactJS)
+  origin: "https://creotech-deploy-frontend.onrender.com", // frontend URI (ReactJS)
 };
 app.use(cors(corsOptions));
 
